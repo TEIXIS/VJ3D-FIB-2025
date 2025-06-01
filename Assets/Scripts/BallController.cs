c81772a293265cd838a7c4ce8c72df9943f95964
Marc Teixidó Sala <marc.teixido.sala@estudiantat.upc.edu>
2025-06-01 17:37:53 +0200

    Powerups

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -97,14 +97,14 @@ public class BallController : MonoBehaviour
         // Inicializamos el diccionario de pesos una sola vez
         powerUpWeights = new Dictionary<GameObject, float>()
         {
-            { magnetPrefab,        10f },
-            { timburrPrefab,        7f },
-            { conkeldurrPrefab,     7f },
-            { slowpokePrefab,       8f },
-            { emberPrefab,          70f },
-            { extraBallPrefab,     12f },
-            { telekinesisPrefab,    4f },
-            { powerBallPrefab,      3f }
+            { magnetPrefab,        7f },
+            { timburrPrefab,        4f },
+            { conkeldurrPrefab,     4f },
+            { slowpokePrefab,       3f },
+            { emberPrefab,          3f },
+            { extraBallPrefab,     5f },
+            { telekinesisPrefab,    3f },
+            { powerBallPrefab,      4f }
         };
     }
 
@@ -180,7 +180,7 @@ public class BallController : MonoBehaviour
         if (attractionActive && v.z > 0f && paddle != null)
         {
             // 2.a) Calcula extents de la paleta
-            var col = paddle.GetComponent<Collider>();
+            var col = paddle.GetComponentInChildren<Collider>();
             float halfW = col.bounds.extents.x;
 
             // 2.b) Distancia horizontal
@@ -565,6 +565,7 @@ public class BallController : MonoBehaviour
         }
             if (r <= acum)
             {
+                spawnPos.y = 10.5f;
                 // Instanciamos este prefab
                 Instantiate(prefab, spawnPos, rotacionAUsar);
                 Debug.Log($"Power-Up generado (ponderado): {prefab.name}");
